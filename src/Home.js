d11bc8b1ac23fd91264d48afa6efa673c6b11b79
gabrielemarchetti00 <gabriele.marchetti26@gmail.com>
2023-05-08 16:27:30 +0200

    add home image and items array

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import './App.css'
+import courtImg from './basketball.jpg'
 
 function Home() {
     return (
       <div>
-        <h1>Home</h1>
+        <h1>Baketball Shop</h1>
+        <img src={courtImg} alt="basketball court" width="1100"/>
       </div>
     );
   }
--- src/Shop.js
+++ src/Shop.js
@@ -1,10 +1,43 @@
 import React from "react";
 import './App.css'
+import uniqid from "uniqid";
+import ballImg from "./images/ball.jpg"
+import bandImg from "./images/band.webp"
+import shirtImg from "./images/shirt.jpg"
+import shoesImg from "./images/shoes.jpg"
 
 function Shop() {
+  const items = [
+    {
+      id: uniqid(),
+      title: 'Shoe',
+      image: shoesImg
+    },
+    {
+      id: uniqid(),
+      title: 'Ball',
+      image: ballImg
+    },
+    {
+      id: uniqid(),
+      title: 'Headband',
+      image: bandImg
+    },
+    {
+      id: uniqid(),
+      title: 'Jersey',
+      image: shirtImg
+    }
+  ]
     return (
       <div>
-        <h1>Shop</h1>
+        <div id="cart-bar">
+          <div>Shopping cart: 0</div>
+          <button>Checkout and Pay</button>
+        </div>
+        <div id="items-section">
+
+        </div>
       </div>
     );
   }
